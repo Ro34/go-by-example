a07287ca1244023bd3b8bbfe5c91de19adc8222f
Ro34 <347629508@qq.com>
2023-07-25 20:12:12 +0800

    input

--- getNum/getnum.go
+++ getNum/getnum.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,5 +15,21 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	secretNum := rand.Intn(maxNum)
 	fmt.Println("num is ", secretNum)
+
+	fmt.Println("input guess")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+	input = strings.TrimSuffix(input, "\n")
+	guess, err := strconv.Atoi(input)
+
+	if err != nil {
+		fmt.Println("err input", err)
+		return
+	}
+	fmt.Println("your guess", guess)
 
 }
