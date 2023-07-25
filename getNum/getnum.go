e4d5bebeff251fe308018fe218f61685af20de5f
Ro34 <347629508@qq.com>
2023-07-25 20:17:23 +0800

    done

--- getNum/getnum.go
+++ getNum/getnum.go
@@ -14,22 +14,36 @@ func main() {
 	maxNum := 100
 	rand.Seed(time.Now().UnixNano())
 	secretNum := rand.Intn(maxNum)
-	fmt.Println("num is ", secretNum)
+	//fmt.Println("num is ", secretNum)
 
 	fmt.Println("input guess")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("err", err)
-		return
-	}
-	input = strings.TrimSuffix(input, "\n")
-	guess, err := strconv.Atoi(input)
 
-	if err != nil {
-		fmt.Println("err input", err)
-		return
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("err", err)
+			return
+		}
+		input = strings.TrimSuffix(input, "\n")
+		guess, err := strconv.Atoi(input)
+
+		if err != nil {
+			fmt.Println("err input", err)
+			return
+		}
+		fmt.Println("your guess", guess)
+
+		if guess > secretNum {
+			fmt.Println("too big")
+			continue
+		} else if guess < secretNum {
+			fmt.Println("too small")
+			continue
+		} else {
+			fmt.Println("yes")
+			break
+		}
 	}
-	fmt.Println("your guess", guess)
 
 }
